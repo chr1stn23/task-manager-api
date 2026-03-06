bd4404375f8a784ab9b86ec5a8e3d8af44b5fa9c
Christian <lvchristian20@gmail.com>
2026-03-06 10:47:57 -0500

    feat: add repositories

---
+++ src/main/java/com/christian/taskmanager/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.christian.taskmanager.repository;
+
+import com.christian.taskmanager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
